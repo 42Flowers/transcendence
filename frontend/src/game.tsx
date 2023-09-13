af4395647a4f38b0393932a0da6422bb3102026a
full tulipe <tulipe@fulls-MacBook-Air.local>
2023-09-13 21:00:18 +0200

    Add game position reset function and call at score change

--- frontend/src/game.tsx
+++ frontend/src/game.tsx
@@ -20,44 +20,46 @@ interface paddleElem {
 }
 
 interface ballElem {
+	direction: number,
+	speed: number,
 	x: number,
 	y: number,
 	size: number,
 }
 
-
-
 //////////////////////////////
 //           GAME           //
 //////////////////////////////
 function Game (props: gameProps) {
-	const canvasRef = useRef<HTMLCanvasElement | null>(null)
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
 	const [score, setScore] = useState<scoreElem>({
 		leftPlayer: '0',
 		rightPlayer: '0',
-	})
+	});
 	
 	// DEPENDING ON SCREEN SIZE:   ball.size, leftPad.width, rightPad.width
 	const [ball, setBall] = useState<ballElem>({
+		direction: 0,
+		speed: 0,
 		x: Math.round(props.width / 2),
 		y: Math.round(props.height / 2),
 	  	size: 15,
-	})
+	});
 	
 	const [leftPad, setLeftPad] = useState<paddleElem>({
 		length: Math.round(props.width / 10),
 		width: 10,
 		x: 5,
 		y: Math.round(props.height / 2) - Math.round(props.width / 20),
-	})
+	});
 	
 	const [rightPad, setRightPad] = useState<paddleElem>({
 		length: Math.round(props.width / 10),
 		width: 10,
 		x: props.width - 15,
 		y: Math.round(props.height / 2) - Math.round(props.width / 20),
-	})
+	});
 	
 	useEffect(() => {
 		document.addEventListener('keydown', handleKeyPress);
@@ -68,10 +70,15 @@ function Game (props: gameProps) {
 	}, [leftPad]);
 	
 	useEffect(() => {
+		resetGamePosition();
+	}, [score]);
+
+	useEffect(() => {
 		window.requestAnimationFrame(renderFrame);
 	}, [ball, leftPad, rightPad]);
-	
+
 	function handleKeyPress(event: KeyboardEvent): void {
+		event.preventDefault;
 		const canvas = canvasRef.current;
 
 		if (canvas == null) {
@@ -79,14 +86,14 @@ function Game (props: gameProps) {
 		}
 
 		if (event.key === 'ArrowUp' && leftPad.y > 0) {
-			console.log('LEFT-UP', leftPad.y)
+			console.log('LEFT-UP', leftPad.y);
 			setLeftPad((pad: paddleElem) => ({
 				...pad,
 				y: pad.y - 10,
 			}));
 		}
 		else if (event.key === 'ArrowDown' && leftPad.y < props.height - leftPad.length) {
-			console.log('LEFT-DOWN', leftPad.y)
+			console.log('LEFT-DOWN', leftPad.y);
 			setLeftPad((pad: paddleElem) => ({
 				...pad,
 				y: pad.y + 10,
@@ -99,41 +106,69 @@ function Game (props: gameProps) {
 		ctx.rect(0, 0, width, height);
 		ctx.fillStyle = 'white';
 		ctx.fill();
-	}
+	};
 	
 	function draw(ctx: CanvasRenderingContext2D): void {
 		
 		// draw ball
-		ctx.fillStyle = 'blue'
-		ctx.beginPath()
-		ctx.arc(ball.x, ball.y, ball.size, 0, 2*Math.PI)
-		ctx.closePath()
-		ctx.fill()
+		ctx.fillStyle = 'blue';
+		ctx.beginPath();
+		ctx.arc(ball.x, ball.y, ball.size, 0, 2*Math.PI);
+		ctx.closePath();
+		ctx.fill();
 		
 		// draw left paddle
-		ctx.fillStyle = 'blue'
-		ctx.fillRect(leftPad.x, leftPad.y, leftPad.width, leftPad.length)
+		ctx.fillStyle = 'blue';
+		ctx.fillRect(leftPad.x, leftPad.y, leftPad.width, leftPad.length);
 		
 		// draw right paddle
-		ctx.fillStyle = 'blue'
-		ctx.fillRect(rightPad.x, rightPad.y, rightPad.width, rightPad.length)
+		ctx.fillStyle = 'blue';
+		ctx.fillRect(rightPad.x, rightPad.y, rightPad.width, rightPad.length);
 
 		ctx.fillStyle = "green";
 		ctx.font = "40px Orbitron";
       	ctx.fillText(score.leftPlayer, Math.round(props.width / 2 / 2), 100);
       	ctx.fillText(score.rightPlayer,Math.round(props.width / 2 * 1.5),100);
-	}
+	};
 	
 	function renderFrame(): void {
 		const context = canvasRef.current?.getContext('2d');
 		if (context != null) {
 			clearBackground(context);
-			draw(context)
+			draw(context);
 		}
-	}
+	};
+
+	function resetGamePosition(): void {
+		setBall({
+			direction: 0,
+			speed: 0,
+			x: Math.round(props.width / 2),
+			y: Math.round(props.height / 2),
+	  		size: 15,
+		});
+		setLeftPad({
+			length: Math.round(props.width / 10),
+			width: 10,
+			x: 5,
+			y: Math.round(props.height / 2) - Math.round(props.width / 20),
+		});
+		setRightPad({
+			length: Math.round(props.width / 10),
+			width: 10,
+			x: props.width - 15,
+			y: Math.round(props.height / 2) - Math.round(props.width / 20),
+		});
+	};
 	
 	return <canvas ref={canvasRef} {...props}/>
 }
+
+// UPDATE SCORE PROTOTYPE
+// setScore((newScore: scoreElem) => ({
+// 	...newScore,
+// 	leftPlayer: (parseInt(newScore.leftPlayer) + 1).toString()
+// }));
 
 export default Game
 

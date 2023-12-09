e35397dfbb9bafb23de4ed4cd93cd5b6d43d2e8d
tulipe <tulipe@fulls-MacBook-Air.local>
2023-12-09 16:24:08 +0100

    Update chat path

---
+++ frontend/src/components/Chat/Menu.tsx
@@ -0,0 +1,58 @@
+import { ChatContext } from "../../contexts/ChatContext";
+import { useContext } from "react";
+
+export interface ChatContextType {
+    chanOrDm: string
+    setChanOrDm: (chanOrDm: string) => void;
+    usersOrBanned: string
+    setUsersOrBanned: (usersOrBanned: string) => void;
+    isDm: boolean
+    setIsDm: (isDm: boolean) => void;
+    currentChannel: number
+    setCurrentChannel: (currentChannel: number) => void;
+    currentDm: number
+    setCurrentDm: (currentDm: number) => void;
+}
+
+type Props = {
+    side: string
+}
+
+const Menu: React.FC<Props> = ({ side }) => {
+    const { setChanOrDm, setUsersOrBanned, setIsDm } = useContext(ChatContext) as ChatContextType;
+    
+
+    const buttonStyle: React.CSSProperties = {
+        width: "50%",
+        height: "100%",
+        backgroundColor: "transparent",
+        border: "none",
+        cursor: "pointer"
+    };
+
+    return (
+        side === 'left'
+            ?
+                <>
+                    <button style={buttonStyle} onClick={() => {
+                        setChanOrDm('channel');
+                        setIsDm(false);
+                    }}
+                    >
+                        Channels</button>
+                    <button style={buttonStyle} onClick={() => {
+                        setChanOrDm('dm');
+                        setIsDm(true);
+                    }}
+                    >
+                        Direct Messages</button>
+                </>
+            :
+                <>
+                    <button style={buttonStyle} onClick={() => setUsersOrBanned('users')}>Users</button>
+                    <button style={buttonStyle} onClick={() => setUsersOrBanned('banned')}>Banned Users</button>
+                </>
+    );
+};
+
+export default Menu;
---
+++ frontend/src/components/Chat/SendMessages.tsx
@@ -0,0 +1,39 @@
+import { useContext, useState } from "react";
+import SocketContext from "../Socket/Context/Context";
+import { ChatContext } from "../../contexts/ChatContext";
+import { ChatContextType } from "./Menu";
+
+
+const SendMessages: React.FC = () => {
+    const inputStyle: React.CSSProperties = {
+        width: "60%",
+        height: "30%",
+        display: "flex",
+    };
+    const { currentChannel } = useContext(ChatContext) as ChatContextType;
+    const { SocketState } = useContext(SocketContext);
+    const [message, setMessage] = useState<string>("");
+
+    const handleSubmitMessage = (event: React.FormEvent) => {
+        event.preventDefault();
+        SocketState.socket?.emit("channelmessage", {channelId: currentChannel, message: message});
+        console.log("submitted message", message);
+    };
+  
+    return (
+        <div style={{ display: "flex", flexDirection: "column", height: "100%", alignItems: "center", justifyContent: "center" }}>
+            <form onSubmit={handleSubmitMessage} style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+                <input
+                    style={inputStyle}
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder="msg"
+                />
+                <button type="submit" style={{ flex: "1 1 auto" }}>SEND</button>
+            </form>
+        </div>
+    );
+};
+
+export default SendMessages;

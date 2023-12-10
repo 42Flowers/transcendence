a2ff2deba48963914cc89dc702f3210dc9a966fe
Hloise <heloisemarco@gmail.com>
2023-12-10 15:20:51 +0100

    changes

--- backend/src/rooms/rooms.service.ts
+++ backend/src/rooms/rooms.service.ts
@@ -357,12 +357,13 @@ export class RoomService {
 	async rmPwd(channelId: number) : Promise<any> {
 		try {
 			const chan = await this.prismaService.channel.update({where: {id: channelId}, data: {password : '', accessMask: 1}});
-			if (chan != null ) {
-				return {status: true};
-			}
-			else {
-				return {status: false, msg: "Couldn't remove password"};
-			}
+			return chan != null ? {status: true} : {status: false, msg: "Couldn't remove password"} 
+			// if (chan != null ) {
+			// 	return {status: true};
+			// }
+			// else {
+			// 	return {status: false, msg: "Couldn't remove password"};
+			// }
 		} catch (err) {throw err}
 	}
 

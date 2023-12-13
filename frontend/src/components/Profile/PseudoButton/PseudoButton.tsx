b0436f03e6b64fddd37e9dfbe93e1c5a9fb09a6b
ChaDutel <cha333mac@gmail.com>
2023-12-13 23:16:15 +0100

    remove pseudo css

--- frontend/src/components/Profile/PseudoButton/PseudoButton.tsx
+++ frontend/src/components/Profile/PseudoButton/PseudoButton.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { TextField } from '@mui/material';
 import { Button } from "@mui/material";
-import MainButton from '../../MainButton/MainButton';
-import './PseudoButton.css';
 
 type PseudoButtonProps = {
     currentPseudo: string;
@@ -19,57 +17,55 @@ const PseudoButton: React.FC<PseudoButtonProps> = ({ onChangePseudo, currentPseu
     }, [ onChangePseudo ]);
 
     return (
-        <div className='Pseudo'>
-            <form onSubmit={handleChangePseudo} className='pseudoClass'>
-                <div className='button'>
-                    <TextField
-                        name="username"
-                        label={currentPseudo}
-                        autoComplete="off"
-                        InputLabelProps={{
-                            style: { 
-                                color: '#7638C7', 
-                                fontSize: '1em', 
-                            }
-                        }}
-                        variant="outlined"
-                        placeholder={currentPseudo}
-                        sx={{
-                            color: '#9747FF',
-                            marginTop: '3vh',
-                            width: '35vw',
-                            minWidth: '200px',
-                            maxWidth: '300px',
-                            '& .MuiOutlinedInput-root': {
-                                '& fieldset': {
-                                borderRadius: '20px',
-                                border: '3px solid #9747FF',
-                                },
-                                '&:hover fieldset': {
-                                    border: '4px solid #7638C7'
-                                },
-                                '&.Mui-focused fieldset': {
-                                    border: '4px solid #7638C7'
-                                },
-                            },
-                        }}
-                    />
-                </div>
-                <div className='button'>
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        sx={{ 
+        <form onSubmit={handleChangePseudo}>
+            <div>
+                <TextField
+                    name="username"
+                    label={currentPseudo}
+                    autoComplete="off"
+                    InputLabelProps={{
+                        style: { 
+                            color: '#7638C7', 
                             fontSize: '1em', 
-                            marginTop: '1.5vh',
-                            fontWeight: '900',
-                        }}
-                    >
-                        CHANGE PSEUDO
-                    </Button>
-                </div>
-            </form>
-        </div>
+                        }
+                    }}
+                    variant="outlined"
+                    placeholder={currentPseudo}
+                    sx={{
+                        color: '#9747FF',
+                        marginTop: '3vh',
+                        width: '35vw',
+                        minWidth: '200px',
+                        maxWidth: '300px',
+                        '& .MuiOutlinedInput-root': {
+                            '& fieldset': {
+                            borderRadius: '20px',
+                            border: '3px solid #9747FF',
+                            },
+                            '&:hover fieldset': {
+                                border: '4px solid #7638C7'
+                            },
+                            '&.Mui-focused fieldset': {
+                                border: '4px solid #7638C7'
+                            },
+                        },
+                    }}
+                />
+            </div>
+            <div>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ 
+                        fontSize: '1em', 
+                        marginTop: '1.5vh',
+                        fontWeight: '900',
+                    }}
+                >
+                    CHANGE PSEUDO
+                </Button>
+            </div>
+        </form>
     )
 };
 

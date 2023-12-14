c060d4cf43c5e938444dca0ed5da2ef515cabd0a
ChaDutel <cha333mac@gmail.com>
2023-12-14 03:10:41 +0100

    add end game popup and button to menu

--- frontend/src/components/Game/GameWrapper.tsx
+++ frontend/src/components/Game/GameWrapper.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import { useSocketEvent } from '../Socket/Context/Context';
 import Game from './Game';
 import "./GameWrapper.css";
+import ReturnMenu from './ReturnMenu/ReturnMenu';
 
 interface wrapperProps {
 	width: number,
@@ -19,31 +20,63 @@ interface playersData {
 }
 
 const GameWrapper: React.FC<wrapperProps> = (props) => {
-	const [ playersData, setPlayersData ] = useState<playersData | null>(null);
+	const [ playersData, setPlayersData ] = useState<playersData>();
+	const [ gameEnded, setGameEnded ] = useState(false);
+	const [ leftScore, setLeftScore ] = useState<string>('0');
+	const [ rightScore, setRightScore ] = useState<string>('0');
 
 	const displayPlayerData = useCallback((data: playersData) => {
 		setPlayersData(data);
 	}, [ setPlayersData ]);
 
+	const endGame = () => {
+		setGameEnded(true);
+	}
+
+	
+	const updatePlayersScore = (newScore: {leftPlayer: string, rightPlayer: string}) => {
+		setLeftScore(newScore.leftPlayer);
+		setRightScore(newScore.rightPlayer);
+	}
+	
 	useSocketEvent('playerData', displayPlayerData);
+	useSocketEvent('gameFinished', endGame);
+	useSocketEvent('updateScore', updatePlayersScore);
 
 	return (
 		<div className="game-wrapper">
-			<div className="display-players" >
-				{playersData &&
-					<>
-						<div style={{color: "white"}}>
-							<p>{playersData.left.pseudo}</p>
-						</div>
-						<div style={{color: "white"}}>
-							<p>{playersData.right.pseudo}</p>
-						</div>
-					</>
-				}
-				{!playersData && <p style={{ color: 'white' }}>Waiting for players data ...</p>}
-			</div>
+			{!gameEnded &&
+				<>
+					<div className="display-players" >
+						{playersData &&
+							<>
+								<div style={{color: "white"}}>
+									<p>{playersData.left.pseudo}</p>
+								</div>
+								<div style={{color: "white"}}>
+									<p>{playersData.right.pseudo}</p>
+								</div>
+							</>
+						}
+						{!playersData &&
+							<>
+								<p style={{ color: 'white' }}>Waiting for players data ...</p>
+							</>
+						}
+					</div>
+					<div className="game-div">
+						<Game className="gameCanvas" width={props.width} height={props.height} specialMode={props.specialMode} />
+					</div>
+				</>
+			}
 			<div className="game-div">
-				<Game className="gameCanvas" width={props.width} height={props.height} specialMode={props.specialMode} />
+				{gameEnded  && 
+					<ReturnMenu
+						leftName={playersData?.left?.pseudo ?? 'Left Player'}
+						rightName={playersData?.right?.pseudo ?? 'Right Player'}
+						leftScore={leftScore}
+						rightScore={rightScore}
+					/> }
 			</div>
 		</div>
 	)
---
+++ frontend/src/components/Game/ReturnMenu/ReturnMenu.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import MainButton from "../../MainButton/MainButton";
+import { useNavigate } from 'react-router-dom';
+import './ReturnMenu.css';
+
+interface Props {
+    leftName: string
+    rightName: string
+    leftScore: string
+    rightScore: string
+}
+
+const ReturnMenu: React.FC<Props> = ({ leftName, rightName, leftScore, rightScore }) => {
+    const navigate = useNavigate();
+
+    const handleReturnMenu = () => {
+        navigate('/');
+	}
+
+    if (Number(leftScore) > Number(rightScore))
+    {
+        return (
+            <div className="returnMenu">
+                <p style={{ marginTop: '40px', }} className="winner">WINNER: {leftName} with {leftScore} points</p>
+                <p style={{ marginTop: '40px', }} className="looser">LOOSER: {rightName} with {rightScore} points</p>
+                <MainButton buttonName='Return to Menu' onClick={() => handleReturnMenu()} />
+            </div>
+        );
+    }
+    else
+    {
+        {
+            return (
+                <div className="returnMenu">
+                    <p style={{ marginTop: '40px', }} className="winner">WINNER: {rightName} with {rightScore} points</p>
+                    <p style={{ marginTop: '40px', }} className="looser">LOOSER: {leftName} with {leftScore} points</p>
+                    <MainButton buttonName='Return to Menu' onClick={() => handleReturnMenu()} />
+                </div>
+            );
+        }
+    }
+};
+
+export default ReturnMenu;
